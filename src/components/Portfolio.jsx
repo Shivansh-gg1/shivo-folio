917ca3f9c2eb19e42ccd3b5f9f15eae19c8f44bb
Shivansh-gg1 <shivanshsharma.ait@gmail.com>
2023-03-22 21:36:21 +0530

    fixed the portfolio images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -23,28 +23,60 @@ const Portfolio = () => {
         />
       </div>
       <div>
-        <img src={web2} alt="2ndimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web2}
+          alt="2ndimg"
+        />
       </div>
       <div>
-        <img src={web3} alt="3rdimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web3}
+          alt="3rdimg"
+        />
       </div>
       <div>
-        <img src={web4} alt="4thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web4}
+          alt="4thimg"
+        />
       </div>
       <div>
-        <img src={web5} alt="5thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web5}
+          alt="5thimg"
+        />
       </div>
       <div>
-        <img src={web6} alt="6thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web6}
+          alt="6thimg"
+        />
       </div>
       <div>
-        <img src={web7} alt="6thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web7}
+          alt="6thimg"
+        />
       </div>
       <div>
-        <img src={web8} alt="6thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web8}
+          alt="6thimg"
+        />
       </div>
       <div>
-        <img src={web9} alt="6thimg" />
+        <img
+          className=" w-1/4 rounded-md duration-200 hover:scale-105"
+          src={web9}
+          alt="6thimg"
+        />
       </div>
     </div>
   );
